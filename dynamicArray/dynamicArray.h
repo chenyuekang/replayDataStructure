162d4d2e3b1617df11d9794ca90c8bd14025881f
chenyuekang <1207867631@qq.com>
2023-12-18 13:29:50 +0800

    [修改]新增动态数组的结点

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -2,5 +2,13 @@
 #define __DYNAMIC_ARRAY_H__
 
 /*避免头文件重复包含*/
+typedef int ELEMENTTYPE;
+
+typedef struct dynamicArray
+{
+    ELEMENTTYPE *data;      /* 数组的空间 */
+    ELEMENTTYPE len;        /* 数组的大小 */
+    ELEMENTTYPE capacity;   /* 数组的容量 */
+} dynamicArray;
 
 #endif //__DYNAMIC_ARRAY_H__

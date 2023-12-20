fe27b515283b686996466018c94c348934837e67
chenyuekang <1207867631@qq.com>
2023-12-20 11:15:33 +0800

    [修改]完善插入元素的API接口

--- Linklist/Linklist.c
+++ Linklist/Linklist.c
@@ -45,18 +45,64 @@ int LinkListInit(LinkList ** pList)
 /*链表头插*/
 int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, 0, val);
 }
 
 /*链表尾插*/
 int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, pList->len, val);
 }
 
 /*链表指定位置插入*/
 int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val)
 {
+    int ret = 0;
+    if(pList == NULL)
+    {
+        return NULL_PTR;
+    }
+
+    if(pos < 0 || pos > pList->len)
+    {
+        return INVALID_ACCESS;
+    }
+
+    /*封装结点*/
+    LinkNode * newNode = (LinkNode *)malloc(sizeof(LinkNode) * 1);
+    if(newNode == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /*清除脏数据*/
+    memset(newNode, 0, sizeof(LinkNode) * 1);
+
+#if 1
+
+#endif
+    /*赋值*/
+    newNode->data = val;
+
+#if 1
+    /*从虚拟头结点开始遍历*/
+    LinkNode * travelNode = pList->head;
+#else
+    LinkNode * travelNode = pList->head->next;
+#endif
+    while(pos)    //pos不为0时进入循环，pos为0不进入循环
+    {
+        travelNode = travelNode->next;     //travelNode从头结点往后移
+        pos--;
+    }
+    /*修改结点指向*/
+    newNode->next = travelNode->next;
+    travelNode->next = newNode;
+
+    /*更新链表的长度*/
+    (pList->len)++;
+
+    return ret;
+
 
 }
 
@@ -87,7 +133,17 @@ int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val)
 /*获取链表的长度*/
 int LinkListGetLength(LinkList * pList, int *pSize)
 {
+    int ret = 0;
+    if(pList == NULL)
+    {
+        return NULL_PTR;
+    }
 
+    if(pSize)
+    {
+        *pSize = pList->len;
+    }
+    return ret;
 }
 
 /*链表的销毁*/
@@ -99,5 +155,6 @@ int LinkListdestory(LinkList * pList)
 /*链表遍历接口*/
 int LinkListForeach(LinkList * pList)
 {
+    
 
 }
